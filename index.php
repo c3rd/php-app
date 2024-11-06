a0513fc3c6a2ed777c527b4c75bea57034f666ae
Celio Maroja <c3rd.dev@gmail.com>
2024-11-06 10:43:38 +0100

    feat: creating login page
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <header>
+        <img src="public/images/logo.svg" alt="Logo" class="logo">
+    </header>
+    <main class="container">
+        <section class="auth-form">
+            <form action="" method="post">
+                <input type="text" id="username" name="username" placeholder="Username" required>
+                <input type="password" id="password" name="password" placeholder="Password" required>
+                <input type="submit" value="Login">
+            </form>
+        </section>
+    </main>
 </body>
 </html>
