3a7ae8ca2dd8711e267f6683ab70617c266f775f
Celio Maroja <c3rd.dev@gmail.com>
2024-11-10 21:18:10 +0100

    refactor: moving flash message to index page
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- login.php
+++ login.php
@@ -1,10 +1,4 @@
-<?php if(isset($_SESSION['message'])) { ?>
-    <div class="flash-message">
-        <p><?php echo $_SESSION['message'] ?></p>
-    </div>
-<?php }    
-    unset($_SESSION['message']);
-?>
+
 <section class="form">
     <form action="authenticate.php" method="post">
         <input class="form-input" type="text" id="username" name="username" placeholder="Username" required>
