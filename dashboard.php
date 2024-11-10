167f5d14f27dcdeadae0afda2820588743c4ecad
Celio Maroja <c3rd.dev@gmail.com>
2024-11-10 10:16:56 +0100

    feat: displaying articles at logged in page
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- dashboard.php
+++ dashboard.php
@@ -1,9 +1,31 @@
 <?php
+require_once './src/domain/IArticleRepository.php';
+require_once './src/infra/repository/ArticleRepository.php';
+require_once './src/infra/controller/ArticleController.php';
+
+$host = getenv('MYSQL_HOST');
+$db = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
 session_start();
 if (empty($_SESSION['username'])) {
     $_SESSION['message'] = 'You need to be logged in to acess';
     header('Location: index.php');
 }
+
+try {
+    $dsn = "mysql:host=$host;dbname=$db";
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "connected";
+} catch (\Throwable $e) {
+    echo "error" . $e->getMessage();
+}
+
+$articleRepository = new \Infra\Repository\ArticleRepository($pdo);
+$articleController = new \Infra\Controller\ArticleController($articleRepository);
+$articles = $articleController->handleRequest();
 
 ?>
 
@@ -20,6 +42,25 @@ if (empty($_SESSION['username'])) {
         <img src="public/images/logo.svg" alt="Logo" class="logo">
     </header>
     <main class="container">
+        <?php if (!empty($articles)) { ?>
+            <section class="form">
+                <h3 class="section-title">All News</h3>
+                <div class="articles">
+                    <?php foreach ($articles as $article): ?>
+                    <div class="article">
+                        <div>
+                            <span><?php echo htmlspecialchars($article['title']); ?></span>
+                            <span><?php echo htmlspecialchars($article['description']); ?></span>
+                        </div>
+                        <div>
+                            <a onclick="editArticle(<?php echo $article['id']; ?>)" href="#">    <img src="public/images/pencil.svg" alt="Edit" width="15" height="15"></a>
+                            <a onclick="deleteArticle(<?php echo $article['id']; ?>)" href="#">    <img src="public/images/close.svg" alt="Edit" width="15" height="15"></a>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </section>
+        <?php } ?>
         <section class="form">
             <h3 class="section-title">Create News</h3>
             <form method="post">
