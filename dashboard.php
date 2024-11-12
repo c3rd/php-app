4ba079f44cf3d56669f78708be64ef1cf4b9a954
Celio Maroja <c3rd.dev@gmail.com>
2024-11-12 10:00:56 +0100

    refactor: adjusting form actions
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- dashboard.php
+++ dashboard.php
@@ -1,15 +1,13 @@
 <?php
+require_once './src/infra/database/DatabaseConnection.php';
 require_once './src/application/repository/IArticleRepository.php';
 require_once './src/infra/repository/ArticleRepository.php';
 require_once './src/infra/controller/ArticleController.php';
 require_once './src/domain/Article.php';
 
-$articleRepository = new \Infra\Repository\ArticleRepository($connection->getPDO());
+$articleRepository = new \Infra\Repository\ArticleRepository();
 $articleController = new \Infra\Controller\ArticleController($articleRepository);
-$articleController->handleRequest();
-
-$articles = $articleRepository->all();
-
+$articles = $articleController->handleRequest();
 ?>
 <?php if (!empty($articles)) { ?>
     <section class="form">
@@ -22,8 +20,12 @@ $articles = $articleRepository->all();
                     <span class="description"><?php echo htmlspecialchars($article['description']); ?></span>
                 </div>
                 <div>
-                    <button onclick="editArticle(<?php echo $article['id']; ?>)">    <img src="public/images/pencil.svg" alt="Edit" width="15" height="15"></button>
-                    <button onclick="deleteArticle(<?php echo $article['id']; ?>)" href="#">    <img src="public/images/close.svg" alt="Edit" width="15" height="15"></button>
+                    <a onclick="editArticle(<?php echo $article['id']; ?>)"><img src="public/images/pencil.svg" alt="Edit" width="15" height="15"></a>
+                    <form action="news.php" method="post">
+                        <input type="hidden" name="article_id" value="<?php echo $article['id']; ?>">
+                        <input type="hidden" name="action" value="delete">
+                        <button type="submit"><img src="public/images/close.svg" alt="Edit" width="15" height="15"></button>
+                    </form>
                 </div>
             </div>
             <?php endforeach; ?>
@@ -35,12 +37,15 @@ $articles = $articleRepository->all();
         <h3 id="form_header" class="section-title">Create News</h3>
         <button id="close_edit_mode" onclick="resetForm()" href=""><img src="public/images/close.svg" alt="Edit" width="15" height="15"></button>
     </div>
-    <form method="post">
-        <input type="hidden" id="article_id" name="article_id">
+    <form action="news.php" method="post">
+        <input type="hidden" id="form_article_id" name="article_id">
         <input class="form-input" type="text" id="title" name="title" placeholder="Title" required>
         <input class="form-input" type="textarea" id="description" name="description" placeholder="Description" required>
         <input class="form-button" type="submit" value="Create">
-        <a href="logout.php" class="form-button">Logout</a>
+    </form>
+    <form action="auth.php" method="post">
+        <input type="hidden" name="action" value="logout">
+        <input type="submit" class="form-button" value="Logout"> 
     </form>
 </section>
 
@@ -53,7 +58,7 @@ $articles = $articleRepository->all();
 
         document.getElementById('title').value = title;
         document.getElementById('description').value = description;
-        document.getElementById('article_id').value = id;
+        document.getElementById('form_article_id').value = id;
         document.getElementById('form_header').textContent = "Edit News";
         document.querySelector('input.form-button[type="submit"]').value = "Save";
         document.querySelector('#close_edit_mode').style.display = 'block'; 
@@ -67,11 +72,5 @@ $articles = $articleRepository->all();
         document.getElementById('form_header').textContent = "Create News";
         document.querySelector('input.form-button[type="submit"]').value = "Create";
         document.querySelector('#close_edit_mode').style.display = 'none';
-    }
-
-    function deleteArticle(id)
-    {
-        fetch(window.location.href + '?id=' + id)
-        .then(data => location.reload());
     }
 </script>
