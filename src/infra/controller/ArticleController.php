44a24c08171eddbe4500a0d28c644373ede7b8c8
Celio Maroja <c3rd.dev@gmail.com>
2024-11-12 11:52:04 +0100

    style: changing how to set the session message
    Signed-off-by: Celio Maroja <c3rd.dev@gmail.com>

--- src/infra/controller/ArticleController.php
+++ src/infra/controller/ArticleController.php
@@ -21,22 +21,23 @@ class ArticleController {
             if (!empty($_POST['article_id']) && empty($_POST['action']))
             {
                 $article = new Article($_POST['article_id'], $_POST['title'], $_POST['description']);
-                $success = $this->articleRepository->update($article);
+                $this->articleRepository->update($article);
                 $message = 'News was changed sucessfully!';
             }
             elseif (!empty($_POST['article_id']) && $_POST['action'] == 'delete')
             {
-                $success = $this->articleRepository->delete($_POST['article_id']);
+                $this->articleRepository->delete($_POST['article_id']);
                 $message = 'News was sucessfully deleted';
             }
             else
             {
                 $article = new Article(null, $_POST['title'], $_POST['description']);
-                $success = $this->articleRepository->create($article);
+                $this->articleRepository->create($article);
                 $message = 'News was successfully created!';
             }
-            $_SESSION['status'] = $success;
-            $_SESSION['message'] = $success ? $message : 'Error Occured';
+
+            $_SESSION['status'] = true;
+            $_SESSION['message'] = $message;
             header('Location: index.php');
         }
         elseif ($_SERVER['REQUEST_METHOD'] == 'GET')
